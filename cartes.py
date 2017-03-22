ecbd2cf6c7f91aeaadaf6d4dc7bf7e99c7a8b3ed
Louis-Saglio <louis.saglio@sfr.fr>
2017-03-22 19:44:11 +0100

    Mise en place de la méthode create_border. Ajout d'une clef position au dictionnaires renvoyés par __iter__ Ajout de trois todo.

--- cartes.py
+++ cartes.py
@@ -1,3 +1,24 @@
+class Case:
+
+    def __init__(self, value, owning_map, coordinates):
+        self.value = value
+        self.owning_map = owning_map
+        self.coordinates = coordinates
+        self.border = self.set_position()
+
+    def __str__(self):
+        return str(self.value)
+
+    def set_position(self):
+        l, h = self.coordinates
+        return {
+            "is_top": True if h == 0 else False,
+            "is_left": True if l == 0 else False,
+            "is_bottom": True if h == self.owning_map.height - 1 else False,
+            "is_right": True if l == self.owning_map.width - 1 else False
+        }
+
+
 class Map:
 
     def __init__(self, width, height, filling='M', border='B'):
@@ -11,46 +32,34 @@ class Map:
         iterator = []
         for h in range(self.height):
             for l in range(self.width):
-                iterator.append({
-                    "case": self.map[l][h],
-                    "position": (l, h),
-                    "is_top_border": True if h == 0 else False,
-                    "is_left_border": True if l == 0 else False,
-                    "is_bottom_border": True if h == self.height-1 else False,
-                    "is_right_border": True if l == self.width-1 else False
-                })
+                iterator.append(self[l, h])
         return iter(iterator)
 
     def __getitem__(self, pos):
-        index1, index2 = pos
-        return self.map[index1][index2]
+        l, h = pos
+        return self.map[l][h]
 
     def __setitem__(self, key, value):
         index1, index2 = key
-        self.map[index1][index2] = value
+        self.map[index1][index2] = Case(value, self, key)
 
     def __str__(self):
         rep = ''
         for case in self:
-            rep += case["case"] + ''
+            rep += str(case.value) + ' ' + ('\n' if case.border["is_right"] else '')
         return rep
 
     def create_map(self):
-        return [[self.filling_type for h in range(self.height)] for l in range(self.width)]
-
-    def print_map(self):
-        print('')
-        for case in self:
-            print(case["case"] + ' ', end='\n' if case["is_right_border"] else '', sep='')
+        return [[Case(self.filling_type, self, (l, h)) for h in range(self.height)] for l in range(self.width)]
 
     def is_border_position(self, pos):
-        h, l = pos
-        return h == self.height - 1 or h == 0 or l == self.width - 1 or l == 0
+        l, h = pos
+        return h == (self.height - 1) or (h == 0) or (l == self.width - 1) or (l == 0)
 
     def create_border(self):
         for case in self:
-            if len([case[clef] for clef in case if case[clef] and clef not in ["case", "position"]]) != 0:
-                self[case["position"]] = self.border_type
+            if self.is_border_position(case.coordinates):
+                self[case.coordinates] = Case(self.border_type, self, case.coordinates)
 
     @staticmethod
     def clear():
@@ -59,14 +68,6 @@ class Map:
         system("cls")
 
 
-class Pawn:
-
-    def __init__(self, a_map, position=(1, 1)):
-        self.x, self.y = position
-
-    def get_in_map(self):
-        pass
-
 if __name__ == '__main__':
     # todo tester le module avec des nombres aléatoires
     # from random import randint
@@ -74,6 +75,5 @@ if __name__ == '__main__':
     test = Map(15, 7)
     test[2, 3] = "P"
     test.create_border()
-    test.print_map()
-    print(test[2, 3])
     print(test)
+    print(test[2, 3])
