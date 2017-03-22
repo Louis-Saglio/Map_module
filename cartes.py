32b6ae612a1bf87bc55eca948c289bf1fbece62d
Louis-Saglio <louis.saglio@sfr.fr>
2017-03-22 22:56:33 +0100

    Mise en place de la méthode create_border. Ajout d'une clef position au dictionnaires renvoyés par __iter__ Ajout de trois todo.

--- cartes.py
+++ cartes.py
@@ -4,12 +4,12 @@ class Case:
         self.value = value
         self.owning_map = owning_map
         self.coordinates = coordinates
-        self.border = self.set_position()
+        self.border = self.get_border_type()
 
     def __str__(self):
         return str(self.value)
 
-    def set_position(self):
+    def get_border_type(self):
         l, h = self.coordinates
         return {
             "is_top": True if h == 0 else False,
@@ -17,6 +17,10 @@ class Case:
             "is_bottom": True if h == self.owning_map.height - 1 else False,
             "is_right": True if l == self.owning_map.width - 1 else False
         }
+
+    def is_border_position(self, pos):
+        l, h = pos
+        return len([True for value in self.border.values() if value]) != 0
 
 
 class Map:
@@ -52,29 +56,69 @@ class Map:
     def create_map(self):
         return [[Case(self.filling_type, self, (l, h)) for h in range(self.height)] for l in range(self.width)]
 
-    def is_border_position(self, pos):
-        l, h = pos
-        return h == (self.height - 1) or (h == 0) or (l == self.width - 1) or (l == 0)
-
     def create_border(self):
         for case in self:
-            if self.is_border_position(case.coordinates):
+            if case.is_border_position(case.coordinates):
                 self[case.coordinates] = Case(self.border_type, self, case.coordinates)
 
-    @staticmethod
-    def clear():
-        from platform import system as get_os_name
-        from os import system
-        system("cls" if get_os_name() == "Windows" else "clear")
+
+class Pawn:
+
+    def __init__(self, owning_map, position, look):
+        """
+        :type owning_map Map
+        :type position tuple
+        :type look str
+        """
+        self._position = position
+        self.look = look
+        self.owning_map = owning_map
+        self._case = self.owning_map[self.position]
+        self.set_position(self.position)
+
+    def __str__(self):
+        return str(self.look)
+
+    @property
+    def case(self):
+        return self._case
+
+    @property
+    def position(self):
+        return self._position
+
+    def set_position(self, new_position):
+        self._position = new_position
+        self._case = self.owning_map[self.position]
+        self.owning_map[self.position] = self
+
+    def move_up(self):
+        self.owning_map[self.position] = self.case
+        self.set_position((self.position[0], self.position[1]-1))
+
+    def move_down(self):
+        self.owning_map[self.position] = self.case
+        self.set_position((self.position[0], self.position[1]+1))
+
+    def move_right(self):
+        self.owning_map[self.position] = self.case
+        self.set_position((self.position[0]+1, self.position[1]))
+
+    def move_left(self):
+        self.owning_map[self.position] = self.case
+        self.set_position((self.position[0]-1, self.position[1]))
 
 
 if __name__ == '__main__':
-    from platform import system as get_os_name
-    from os import system
     # todo mesurer le temps d'execution
     x, y, a, b = 10, 6, 2, 3
     test = Map(x, y)
     test.create_border()
     test[a, b] = "P"
-    print(test)
     print(test[a, b])
+    pawn = Pawn(test, (5, 4), "O")
+    pawn.move_up()
+    pawn.move_left()
+    pawn.move_down()
+    pawn.move_right()
+    print(test)
