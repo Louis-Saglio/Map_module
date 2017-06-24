c6963aebf458ccd0a6de4a414ca6423e94d2e589
Louis-Saglio <louis.saglio@ynov.com>
2017-06-24 17:25:59 +0200

    Meilleur séparation de Case et Pawn

--- cartes.py
+++ cartes.py
@@ -1,3 +1,6 @@
+from pprint import pprint
+
+
 class Case:
 
     def __init__(self, value, owning_map, coordinates):
@@ -5,9 +8,15 @@ class Case:
         self.owning_map = owning_map
         self.coordinates = coordinates
         self.border = self.get_border_type()
+        self.pawn = {"is_pawn": False, "look": None}
 
     def __str__(self):
+        if self.pawn["is_pawn"]:
+            return str(self.pawn["look"])
         return str(self.value)
+
+    def __repr__(self):
+        return self.__str__()
 
     def get_border_type(self):
         l, h = self.coordinates
@@ -21,6 +30,14 @@ class Case:
     def is_border_position(self):
         return len([None for value in self.border.values() if value]) != 0
 
+    def get_case_by_vector(self, *coor):
+        # Gérer les cas où les cases sont en dehors du plateau
+        position = self.coordinates[0] + coor[0], self.coordinates[1] + coor[1]
+        return self.owning_map[position]
+
+    def get_connected(self):
+        return [self.get_case_by_vector(vector) for vector in self.owning_map.directions.items()]
+
 
 class Map:
 
@@ -30,6 +47,7 @@ class Map:
         self.border_type = border
         self.filling_type = filling
         self.map = self.create_map()
+        self.directions = {"up": (0, -1), "down": (0, 1), "right": (1, 0), "left": (-1, 0)}
 
     def __iter__(self):
         iterator = []
@@ -49,8 +67,21 @@ class Map:
     def __str__(self):
         rep = ''
         for case in self:
-            rep += str(case.value) + ' ' + ('\n' if case.border["is_right"] else '')
+            rep += str(case) + ' ' + ('\n' if case.border["is_right"] else '')
         return rep
+
+    def __repr__(self):
+        return str(self).replace('\n', '\\n ')
+
+    def __contains__(self, item):
+        if isinstance(item, tuple):
+            return 0 <= item[0] <= self.width and 0 <= item[1] <= self.height
+        elif isinstance(item, Case):
+            return self[item.coordinates] is item
+        elif isinstance(item, Pawn):
+            return item.case in self
+        else:
+            raise NotImplementedError
 
     def create_map(self):
         return [[Case(self.filling_type, self, (l, h)) for h in range(self.height)] for l in range(self.width)]
@@ -58,21 +89,20 @@ class Map:
     def create_border(self):
         for case in self:
             if case.is_border_position():
-                self[case.coordinates] = Case(self.border_type, self, case.coordinates)
+                    case.value = self.border_type
 
 
 class Pawn:
 
-    def __init__(self, owning_map, position, look):
+    def __init__(self, owning_map, case, look):
         """
         :type owning_map Map
         :type position tuple
         :type look str
         """
-        self._position = position
         self.look = look
         self.owning_map = owning_map
-        self._case = self.owning_map[self.position]
+        self._case = case
 
     def __str__(self):
         return str(self.look)
@@ -81,20 +111,17 @@ class Pawn:
     def case(self):
         return self._case
 
-    @property
-    def position(self):
-        return self._position
-
-    @position.setter
-    def position(self, new_position):
-        self._position = new_position
-        self._case = self.owning_map[self.position]
-        self.owning_map[self.position] = self
+    @case.setter
+    def case(self, new_case: Case):
+        self._case.pawn["is_pawn"] = False
+        self._case.pawn["look"] = self._case.value
+        self._case = self.owning_map[new_case.coordinates]
+        self._case.pawn["is_pawn"] = True
+        self._case.pawn["look"] = self.look
 
     def move(self, direction):
-        directions = {"up": (0, -1), "down": (0, 1), "right": (1, 0), "left": (-1, 0)}
-        self.owning_map[self.position] = self.case
-        self.position = (self.position[0]+directions[direction][0], self.position[1]+directions[direction][1])
+        vector = self.owning_map.directions[direction]
+        self.case = self._case.get_case_by_vector(*vector)
 
 
 if __name__ == '__main__':
@@ -103,13 +130,19 @@ if __name__ == '__main__':
     debut = time()
     test = Map(x, y)
     test.create_border()
-    test[a, b] = "P"
-    pawn = Pawn(test, (5, 4), "O")
+    # test[a, b] = "P"
+    pawn = Pawn(test, test[6, 3], "O")
     pawn.move("up")
     pawn.move("left")
     pawn.move("down")
     pawn.move("right")
     pawn.move("right")
     pawn.move("up")
+    assert test[2, 2].get_case_by_vector(1, 1) is test[3, 3]
+    assert test[0, 2] in test
+    assert (0, 5) in test
+    assert pawn in test
+    # Si la séquence de pawn.move est modifié, ce test doit être déplacé
+    assert pawn.case.coordinates == (7, 2)
     print(test)
     print("Tests executés en", round(time()-debut, 3), "seconde(s) avec succès.")
